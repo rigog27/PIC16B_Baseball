9c08c585c01ff1df2178d2aac6b1d7a2335298c2
harryzjiang8 <harryzjiang@gmail.com>
2025-11-29 22:40:27 -0800

    put styles into css file and add list of batters

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    df = pd.read_csv("data/yama_pitching_data")
+    unique_batters = df['batter_name'].unique()
+    return render_template("index.html", batters=unique_batters)
 
 if __name__ == '__main__':
     app.run(debug=True)

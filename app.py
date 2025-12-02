5d49337c1463275d8c24cb7f8e9111abb51ba477
harryzjiang8 <harryzjiang@gmail.com>
2025-12-02 13:09:13 -0800

    add some css styling to make page look better and get prediction page ready

--- app.py
+++ app.py
@@ -10,5 +10,20 @@ def index():
     unique_batters = df['batter_name'].unique()
     return render_template("index.html", batters=unique_batters)
 
+@app.route("/prediction", methods=["GET", "POST"])
+def prediction():
+    batter = request.form.get("batter")
+    outs = request.form.get("outs")
+    strikes = request.form.get("strikes")
+    balls = request.form.get("balls")
+    on_1b = request.form.get("on_1b")
+    on_2b = request.form.get("on_2b")
+    on_3b = request.form.get("on_3b")
+
+    return render_template("prediction.html", outs=outs,
+                           strikes=strikes, batter=batter, balls=balls,
+                           on_1b=on_1b, on_2b=on_2b, on_3b=on_3b)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
